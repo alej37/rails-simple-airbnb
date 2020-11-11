b7648ab02ca766ee3394703eef08aca5f7956da5
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2020-11-11 16:39:16 -0500

    New and Create added and working

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,29 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flats_params)
+    @flat.save
+    redirect_to flats_path
+  end
+
+
+
+
+
+
+  private
+
+  def flats_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
