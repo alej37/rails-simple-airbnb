cbc4850050de4f0ff06a127c84a90bd679a19526
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2020-11-11 16:52:16 -0500

    All controller methods working, missing frontend

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -17,10 +17,21 @@ class FlatsController < ApplicationController
     redirect_to flats_path
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
 
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flats_params)
+    redirect_to flats_path
+  end
 
-
-
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
 
   private
 
